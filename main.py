a97d22a19aa9c3972d740c822606cf99b457e997
Sanjeev Easparan <easparansanjeev@gmail.com>
2020-10-30 15:51:55 -0400

    added input

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import tkinter
 
 gui = tkinter.Tk()
-
-count = 0
-
-def click():
-    global count
-    count += 1
-    Count = tkinter.Label(gui, text=str(count))
-    Count.pack()
-
-add = tkinter.Button(gui, text="Add Contact", command=click, fg="white", bg="blue")
-add.pack()
-
 gui.title('Contact Book')
 gui.geometry('500x500+0+0')
+
+name = tkinter.Entry(gui, width=20)
+name.pack()
+
+def username():
+    title = name.get() + "'s Contacts"
+    Name = tkinter.Label(gui, text=title)
+    Name.pack()
+
+add = tkinter.Button(gui, text="Continue", command=username, fg="white", bg="blue")
+add.pack()
+
+
 gui.mainloop()
