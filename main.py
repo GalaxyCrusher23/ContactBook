6125eabf21eafaa281a67cd63684903fc008d856
Sanjeev Easparan <easparansanjeev@gmail.com>
2020-10-29 21:37:25 -0400

    Created window and added button with simple function

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 import tkinter
 
+gui = tkinter.Tk()
+
+count = 0
+
+def click():
+    global count
+    count += 1
+    Count = tkinter.Label(gui, text=str(count))
+    Count.pack()
+
+add = tkinter.Button(gui, text="Add Contact", command=click)
+add.pack()
+
+gui.title('Contact Book')
+gui.geometry('500x500+0+0')
+gui.mainloop()
