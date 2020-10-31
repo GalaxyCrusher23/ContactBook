d882720c1489eec133686726d0fdc00a24927848
Sanjeev Easparan <easparansanjeev@gmail.com>
2020-10-31 13:45:45 -0400

    Added font changes and disabled resizing for application

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 import tkinter
+import tkinter.font as tkFont
+from PIL import ImageTk, Image
 
 gui = tkinter.Tk()
+
+#Gives title to application
 gui.title('Contact Book')
+
+#Sets the position and size of the application
 gui.geometry('500x500+0+0')
-gui.iconbitmap("./PalPadSprite.ico")
+
+#Change the icon of the application
+gui.iconbitmap("PalPadSprite.ico")
+
+#Changes style and font of text
+title_fontStyle = tkFont.Font(family="Lucida Grande", size=32)
+
+username_title = tkinter.Label(gui, text="What is your name?", font=title_fontStyle)
+username_title.pack()
+
+#To add any image
+#img = ImageTk.PhotoImage(Image.open(""))
 
 '''#This is for later use
 name = tkinter.Entry(gui, width=20)
@@ -19,5 +36,5 @@ add.pack()
 '''
 
 
-
+gui.resizable(False, False)
 gui.mainloop()
