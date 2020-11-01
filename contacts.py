1d0c7e92c1ee2210486ff8353eed3630da42ff07
Sanjeev Easparan <easparansanjeev@gmail.com>
2020-11-01 11:11:39 -0500

    Added an plus image, now able to freely move text and images

--- contacts.py
+++ contacts.py
@@ -1,6 +1,13 @@
 import tkinter
 import tkinter.font as tkFont
 from PIL import ImageTk, Image
+#Need to install Pillow by using pip install
+
+def add():
+    pass
+
+def select():
+    pass
 
 gui = tkinter.Tk()
 
@@ -15,15 +22,21 @@ gui.iconbitmap("PalPadSprite.ico")
 
 
 #Changes style and font of text
-title_fontStyle = tkFont.Font(family="Lucida Grande", size=32)
+title_fontStyle = tkFont.Font(family="Lucida Grande", size=24)
 
 username_title = tkinter.Label(gui, text="Sanjeev's Contacts", font=title_fontStyle)
-username_title.pack()
+#username_title .grid(row=0, column=250)
+username_title.place(x=112.5, y=0)
 
 contacts = []
 
-def add():
-    pass
+img = ImageTk.PhotoImage(Image.open("AddContact.png"))
+plus = tkinter.Label(gui, image=img)
+#plus.image = img
+plus.place(x=350, y=350)
+#tkinter.Label(image=img).grid(row=390, column=390)
+
+
 #To add any image
 #img = ImageTk.PhotoImage(Image.open(""))
 
