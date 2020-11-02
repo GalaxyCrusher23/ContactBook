fc4490fa6ac8a99cf2ceacbb3c6e3e5902029f26
Sanjeev Easparan <easparansanjeev@gmail.com>
2020-11-01 19:14:00 -0500

    Made edits to the pop-up window, still does nothing at the moment

--- contacts.py
+++ contacts.py
@@ -20,26 +20,36 @@ def add():
     add_contact.geometry("300x200")
     add_contact.iconbitmap("PalPadSprite.ico")
 
-    global add
-    add = tkinter.Entry(add_contact, width=30)
-    add.pack()
+    global name
+    name = tkinter.Entry(add_contact, width = 20)
+    name.place(x = 112.5, y = 75)
+
+    name_label = tkinter.Label(add_contact, text = "Name: ")
+    name_label.place(x = 10, y = 75)
+
+    global num
+    num = tkinter.Entry(add_contact, width = 20)
+    num.place(x = 112.5, y = 125)
+
+    num_label = tkinter.Label(add_contact, text = "Phone Number: ")
+    num_label.place(x = 10, y = 125)
 
 def select():
     pass
 
 #Changes style and font of text
-title_fontStyle = tkFont.Font(family="Lucida Grande", size=24)
+title_fontStyle = tkFont.Font(family = "Lucida Grande", size = 24)
 
-username_title = tkinter.Label(gui, text="Sanjeev's Contacts", font=title_fontStyle)
+username_title = tkinter.Label(gui, text = "Sanjeev's Contacts", font = title_fontStyle)
 #username_title .grid(row=0, column=250)
-username_title.place(x=112.5, y=0)
+username_title.place(x = 112.5, y = 0)
 
 contacts = []
 
-img = tkinter.PhotoImage(file="AddContact.png")
-plus_img = tkinter.Label(image=img)
-plus = tkinter.Button(gui, image=img, command=add, borderwidth=0)
-plus.place(x=350, y=350)
+img = tkinter.PhotoImage(file = "AddContact.png")
+plus_img = tkinter.Label(image = img)
+plus = tkinter.Button(gui, image = img, command = add, borderwidth = 0)
+plus.place(x = 350, y = 350)
 
 
 gui.resizable(False, False)
