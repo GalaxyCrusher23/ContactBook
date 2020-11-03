acb4e07869decf6996f7f7669324e718e8f68dbe
Sanjeev Easparan <easparansanjeev@gmail.com>
2020-11-03 08:50:15 -0500

    Changed contact_list from Button to Listbox, attempted to make a Sort function (did not work)

--- contacts.py
+++ contacts.py
@@ -1,7 +1,7 @@
 import tkinter
 import tkinter.font as tkFont
 from tkinter import messagebox
-from tkinter import IntVar
+from tkinter import *
 
 contacts = []
 
@@ -21,11 +21,10 @@ gui.iconbitmap("PalPadSprite.ico")
 title_fontStyle = tkFont.Font(family = "Lucida Grande", size = 24, weight = tkFont.BOLD, underline = True)
 contact_fontStyle = tkFont.Font(family = "Times", size = 14)
 
-contact_list = tkinter.Button(gui)
+contact_list = tkinter.Listbox(gui, font = contact_fontStyle, fg = "blue", height = 18, width = 36)
+contact_list.place(x = 5, y = 75)
 
 def add():
-    contactx = 5
-    contacty = 75
     if len(name.get()) > 0 and len(num.get()) > 0:
         try:
             int(num.get())
@@ -34,20 +33,15 @@ def add():
             raise 
         global contact_list
         contacts.append([name.get(), num.get()])
-        contacts.sort()
-        #print(contacts)
-        contact_list.destroy()
+        contact_list.delete(0, END)
         for i in range(len(contacts)):
-            contact_list = tkinter.Button(gui, text = contacts[i][0] + " (" + contacts[i][1] + ")", font = contact_fontStyle, fg = "blue", command = selectContact, borderwidth = 0)
-            contact_list.place(x = contactx, y = contacty + i*30 )
+            contact_list.insert(END, contacts[i][0] + " (" + contacts[i][1] + ")")
         add_contact.destroy()
     else:
        messagebox.showerror("Error", "Requirements have not been filled")
         
 def remove():
     response = messagebox.askyesno("Remove Contact?", "Are you sure you want to remove this contact?")
-    #if response == 1:
-    #    contacts.remove()
     
 
 def AddContact():
@@ -94,6 +88,12 @@ def selectContact():
     removeButton = tkinter.Button(select_contact, text = "Remove", fg = "blue", borderwidth = 0, command = remove)
     removeButton.place(x = 222, y = 225)
 
+def sortList(l):
+    if sort.get() == 1:
+        return l[0]
+    else:
+        return l[1]
+
 def sortContacts():
     global sort_contact
     sort_contact = tkinter.Toplevel(gui)
@@ -101,11 +101,15 @@ def sortContacts():
     sort_contact.geometry("200x200")
     sort_contact.iconbitmap("PalPadSprite.ico")
 
+    global sort
     sort =  IntVar()
     sort.get()
 
     tkinter.Radiobutton(sort_contact, text = "Name", variable = sort, value = 1).place(x = 0, y = 0)
     tkinter.Radiobutton(sort_contact, text = "Number", variable = sort, value = 2).place(x = 0, y = 50)
+    
+    contacts.sort(key = sortList)
+    
 
 username_title = tkinter.Label(gui, text = "Sanjeev's Contacts", font = title_fontStyle)
 username_title.place(x = 112.5, y = 0)
